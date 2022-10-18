6b3268c1ad544517795d953d8c63655f90fbd337
Nishant <nishant.mogha@firewires.in>
2022-10-18 10:42:14 +0530

    animate dark mode toggle

--- src/components/DarkModeToggle.tsx
+++ src/components/DarkModeToggle.tsx
@@ -1,18 +1,34 @@
 import { useColorScheme } from "nativewind"
 import { TouchableOpacity } from "react-native"
 import FeatherIcons from "@expo/vector-icons/Feather"
+import Animated, {
+  useAnimatedStyle,
+  useSharedValue,
+  withSpring,
+} from "react-native-reanimated"
 
-interface IProps {}
-
-function DarkModeToggle({}: IProps) {
+function DarkModeToggle() {
   const { colorScheme, toggleColorScheme } = useColorScheme()
+
+  const x = useSharedValue(0)
+  const animStyles = useAnimatedStyle(() => ({
+    transform: [{ rotate: `${x.value * Math.PI}rad` }],
+  }))
+
   return (
-    <TouchableOpacity onPress={toggleColorScheme}>
-      <FeatherIcons
-        name={colorScheme === "dark" ? "moon" : "sun"}
-        style={{ color: colorScheme === "dark" ? "white" : "black" }}
-        size={32}
-      />
+    <TouchableOpacity
+      onPress={() => {
+        x.value = withSpring(x.value + 1)
+        toggleColorScheme()
+      }}
+    >
+      <Animated.View style={animStyles}>
+        <FeatherIcons
+          name={colorScheme === "dark" ? "moon" : "sun"}
+          style={{ color: colorScheme === "dark" ? "white" : "black" }}
+          size={32}
+        />
+      </Animated.View>
     </TouchableOpacity>
   )
 }
